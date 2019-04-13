2319f3482539fb4bc769e4dfdaeeae25baf97f98
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2019-04-13 23:34:03 +0300

    ConsolePainter added

---
+++ Lab1_FileManager/Lab1_FileManager/Infrastructure/ConsolePainter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1_FileManager.Infrasctruture
+{
+    public class ConsolePainter
+    {
+        public ConsolePainter()
+        {
+
+        }
+
+        public void DrawLine(int x, int y, string content)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(content);
+        }
+
+        public void DrawFolderFiles(int x, int y, List<string> folderFiles)
+        {
+            foreach (var file in folderFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
