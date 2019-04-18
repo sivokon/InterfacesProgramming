255ddf88513c534bebf33399e022e9f2e4e70463
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2019-04-19 01:36:18 +0300

    Calculator project added (Windows forms)

---
+++ Lab2_Calculator/Lab2_Calculator/Lab2_Calculator/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Lab2_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
